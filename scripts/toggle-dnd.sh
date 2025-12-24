157d38c08b0e45e8e15693323f128afa981f2ce5
Deeptanshu Kumar <70989817+deeptanshukumar@users.noreply.github.com>
2025-12-24 12:13:03 +0530

    Delete scripts directory

--- scripts/toggle-dnd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-current=$(gsettings get org.gnome.desktop.notifications show-banners)
-if [ "$current" = "true" ]; then
-    gsettings set org.gnome.desktop.notifications show-banners false
-else
-    gsettings set org.gnome.desktop.notifications show-banners true
-fi
-
